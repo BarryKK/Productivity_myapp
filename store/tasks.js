406285ba8dcce0cb3a400c833d0117cc7772506d
pyk <1748418812@qq.com>
2020-11-11 20:18:02 +0800

    update

--- store/tasks.js
+++ store/tasks.js
@@ -1,18 +1,41 @@
+import axios from 'axios'
+
+const API_URL="http://localhost:8000/"
+
 export const state = () => ({
-    list: [{text:'Test', done: false}]
-  })
-  
-  export const mutations = {
-    add(state, text) {
-      state.list.push({
-        text,
-        done: false
-      })
-    },
-    remove(state, { todo }) {
-      state.list.splice(state.list.indexOf(todo), 1)
-    },
-    toggle(state, todo) {
-      todo.done = !todo.done
-    }
+  tasks: []
+});
+
+export const actions = {
+  async getAllTasks({commit}){
+    const res = await axios.get(`${API_URL}/tasks/`)
+    commit('set', res.data)
+   }
+}
+
+export const mutations = {
+  set(state, tasks){
+    state.tasks = tasks
+  },
+  add(state, text) {
+    state.tasks.push({
+      text,
+      done: false
+    });
+  },
+  edit(state, payload) {
+    const { index, text } = payload;
+
+    const updatedTask = {
+      ...state.tasks[index],
+      text
+    };
+    state.tasks.splice(index, 1, updatedTask);
+  },
+  remove(state, index) {
+    state.tasks.splice(index, 1);
+  },
+  toggle(state, todo) {
+    todo.done = !todo.done;
   }
+};
